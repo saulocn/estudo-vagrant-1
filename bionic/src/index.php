b6d9523b1c9962a854d166b90de347b026a279bf
Saulo de Carvalho Neto <saulocn@gmail.com>
2019-11-30 00:32:26 -0300

    Provisionando PHP e MySQL

---
+++ bionic/src/index.php
@@ -0,0 +1,19 @@
+<?php 
+echo "Funcionou!"
+?>
+
+<?php
+echo "Testando conexao <br /> <br />";
+$servername = "192.168.3.250";
+$username = "phpuser";
+$password = "pass";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+echo "Conectado com sucesso";
+?>
